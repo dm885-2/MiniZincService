8ea9fa7338b149da29ffbbfa2d31a436bb019565
Michael <clonex_kontakt@hotmail.com>
2021-12-21 18:16:29 +0100

    Removed MySQL connection

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,5 @@
 import rapid from "@ovcina/rapidriver";
 import jwt from "jsonwebtoken";
-import mysql from "mysql";
-
 
 const SECRET = process.env.SECRET ?? `3(?<,t2mZxj$5JT47naQFTXwqNWP#W>'*Kr!X!(_M3N.u8v}%N/JYGHC.Zwq.!v-`;  // JWT secret
 const rabbitUser = process.env.rabbitUser ?? "guest";
@@ -46,18 +44,6 @@ export const host = "amqp://" + rabbitUser + ":" + rabbitPass + "@" + (process.e
 export function getTokenData(token)
 {
     return new Promise(resolve => jwt.verify(token, SECRET, (err, data) => resolve(err ? false : data)));
-}
-
-let connection;
-if(process.env.mysqlDb)
-{
-    connection = mysql.createConnection({
-        host     : process.env.mysqlHost ?? 'localhost',
-        user     : process.env.mysqlUser ?? 'root',
-        password : process.env.mysqlPass ?? '',
-        database : process.env.mysqlDb ?? 'db',
-    });
-    connection.connect();
 }
 
 /**

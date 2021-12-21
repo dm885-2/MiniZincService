7301bef63db640ea20e736c3379cf137cd5d7203
smessie <smessie@smessie.com>
2021-12-21 18:17:47 +0100

    Add async to jest tests

--- src/solver.test.js
+++ src/solver.test.js
@@ -5,60 +5,60 @@ describe("A solver", () => {
     });
 });
 
-// import child from "child_process";
+import child from "child_process";
 
-// import Solver from "./Solver.js";
+import Solver from "./Solver.js";
 
-// let testOutput = `no. of banana cakes = 0
-// no. of chocolate cakes = 0
-// ----------
-// no. of banana cakes = 1
-// no. of chocolate cakes = 0
-// ----------
-// no. of banana cakes = 2
-// no. of chocolate cakes = 0
-// ----------
-// no. of banana cakes = 3
-// no. of chocolate cakes = 0
-// ----------
-// no. of banana cakes = 2
-// no. of chocolate cakes = 1
-// ----------
-// no. of banana cakes = 3
-// no. of chocolate cakes = 1
-// ----------
-// no. of banana cakes = 2
-// no. of chocolate cakes = 2
-// ----------
-// ==========`;
+let testOutput = `no. of banana cakes = 0
+no. of chocolate cakes = 0
+----------
+no. of banana cakes = 1
+no. of chocolate cakes = 0
+----------
+no. of banana cakes = 2
+no. of chocolate cakes = 0
+----------
+no. of banana cakes = 3
+no. of chocolate cakes = 0
+----------
+no. of banana cakes = 2
+no. of chocolate cakes = 1
+----------
+no. of banana cakes = 3
+no. of chocolate cakes = 1
+----------
+no. of banana cakes = 2
+no. of chocolate cakes = 2
+----------
+==========`;
 
-// let ret = (callback) => ({
-//     pid: 0,
-//     stdout: {
-//         on: (type, func) => {
-//             func(testOutput);
-//             callback(null, true, false);
-//         }
-//     }
-// });
+let ret = (callback) => ({
+    pid: 0,
+    stdout: {
+        on: (type, func) => {
+            func(testOutput);
+            callback(null, true, false);
+        }
+    }
+});
 
-// child.exec = jest.fn();
-// child.exec.mockImplementation((cmd, options, callback) => ret(callback));
+child.exec = jest.fn();
+child.exec.mockImplementation((cmd, options, callback) => ret(callback));
 
-// describe("A solver", () => {
-//     beforeEach(() => {
-//         child.exec.mockClear();
-//     });
+describe("A solver", () => {
+    beforeEach(() => {
+        child.exec.mockClear();
+    });
 
-//     it("should return a optimal solution", () => new Solver(false, false, false, false, false, false, false, false,false, false, (data) => {
-//         expect(data.find(d => d.optimal)).toBeTruthy();
-//     }));
+    it("should return a optimal solution", async () => new Solver(false, false, false, false, false, false, false, false,false, false, (data) => {
+        expect(data.find(d => d.optimal)).toBeTruthy();
+    }));
 
-//     it("should return 7 solutions", () => new Solver(false, false, false, false, false, false, false, false,false, false, (data) => {
-//         expect(data.length).toBe(7);
-//     }));
+    it("should return 7 solutions", async () => new Solver(false, false, false, false, false, false, false, false,false, false, (data) => {
+        expect(data.length).toBe(7);
+    }));
 
-//     it("should call the exec function", () => new Solver(false, false, false, false, false, false, false, false,false, false, (data) => {
-//         expect(child.exec).toHaveBeenCalledTimes(1);
-//     }));
-// });
+    it("should call the exec function", async () => new Solver(false, false, false, false, false, false, false, false,false, false, (data) => {
+        expect(child.exec).toHaveBeenCalledTimes(1);
+    }));
+});
